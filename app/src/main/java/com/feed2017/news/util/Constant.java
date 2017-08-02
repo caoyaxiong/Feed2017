bcf09c2de0a9af0d3b30d193f5e334a53aaedfdd
tongchengtianqi <1124564003@qq.com>
2017-08-02 10:03:21 +0800

    Initial commit

---
+++ app/src/main/java/com/feed2017/news/util/Constant.java
@@ -0,0 +1,11 @@
+package com.feed2017.news.util;
+
+public class Constant {
+	
+//	public static String URL_HOME_PAGE = "http://tpms.mb.qq.com/?aid=site_navigation&cid=site_navigation_v3";
+public static String URL_HOME_PAGE = "http://m.adline.com.cn/";
+	//public static String URL_HOME_PAGE = "file:///android_asset/main.html";
+	public static String URL_SEARCH_KEYWORD = "http://m.baidu.com/s?word=";
+	public static String DOWNLOAD_DIR = "S-Browser/downloads";
+
+}
